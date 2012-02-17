841781ac7bec4a45aee78913e522ab4b0e3e2c62
asunshinehill <aimee@ninthlink.com>
2012-02-17 22:41:01 +0000

    Let's try that again with hiding the sitemap link...

--- sites/all/themes/m2m2/template.php
+++ sites/all/themes/m2m2/template.php
@@ -110,13 +110,15 @@ function phptemplate_get_ie_styles() {
  * @ingroup themeable
  */
 function phptemplate_site_map_menu_item($link, $has_children, $menu = '', $in_active_trail = FALSE, $extra_class = NULL) {
+  if($class=='active'){
+      return;
+  }
   $class = ($menu ? 'expanded' : ($has_children ? 'collapsed' : 'leaf'));
   if (!empty($extra_class)) {
     $class .= ' '. $extra_class;
   }
   if ($in_active_trail) {
-      return;
-    //$class .= ' active-trail';
+    $class .= ' active-trail';
   }
   return '<li class="'. $class .'">'. $link . $menu ."</li>\n";
 }
