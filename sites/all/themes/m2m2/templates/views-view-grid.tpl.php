4b5d6fb89334bb8e283f50ea750ffd15d4bde59d
alex chousmith <alex@ninthlink.com>
2012-02-21 21:47:28 +0000

    update to grid for a min-height

--- sites/all/themes/m2m2/templates/views-view-grid.tpl.php
+++ sites/all/themes/m2m2/templates/views-view-grid.tpl.php
@@ -11,23 +11,12 @@
  */
 ?>
 <?php if (!empty($title)) : ?>
-  <div class="featured_search_header">
-    <h3><?php print $title; ?></h3>
-  <?php if(!(count($view->exposed_input['field_featured_product_category_value_many_to_one']) == 1 && $view->exposed_input['field_featured_product_category_value_many_to_one'][0] == $title)){?>
-    <a href="#" onClick="var form = $('#views-exposed-form-featured-products-page-1'); form.find('input:checkbox[value!=\'<?php echo str_replace('&', '&amp;', $title);?>\']').attr('checked', false).parent().removeClass('highlight');form.find('.bef-checkboxes input[value=\'<?php echo str_replace('&', '&amp;', $title);?>\']').attr('checked', true).parent().addClass('highlight');form.submit();return false;">View All</a>
-    <?php $display_none = TRUE;?>
-  <?php }else{
-      $display_none = FALSE;
-  }?>
-  </div>
+  <h3><?php print $title; ?></h3>
 <?php endif; ?>
 <table class="<?php print $class; ?>"<?php print $attributes; ?>>
   <tbody>
     <?php foreach ($rows as $row_number => $columns): ?>
       <?php
-        if($display_none && $row_number > 0){
-          break;
-        }
         $row_class = 'row-' . ($row_number + 1);
         if ($row_number == 0) {
           $row_class .= ' row-first';
@@ -36,7 +25,7 @@
           $row_class .= ' row-last';
         }
       ?>
-      <tr class="<?php print $row_class; ?><?php echo ($row_number != 0 ? ' row-show-more' : '');?>">
+      <tr class="<?php print $row_class; ?>">
         <?php foreach ($columns as $column_number => $item): ?>
           <td class="<?php print $column_classes[$row_number][$column_number]; ?>">
             <div class="fields_outer">
