f9dc637e9732c368d9303079deaa951e1757903b
alex chousmith <alex@ninthlink.com>
2012-04-19 17:26:38 +0000

    was missing JS on the FEATURED PRODUCT page

--- sites/all/themes/m2m2/templates/views-view--featured-products--page.tpl.php
+++ sites/all/themes/m2m2/templates/views-view--featured-products--page.tpl.php
@@ -28,7 +28,16 @@
  * @ingroup views_templates
  */
 ?>
-<script>
+<script type="text/javascript">
+  function toggle_devices(view_details){
+    if(view_details){
+      $('#device_details').show();
+      $('#results').hide();
+    }else{
+      $('#device_details').hide();
+      $('#results').show();
+    }
+  }
   $(document).ready(function(){
     $('.view-content .field-content a').click(function() {
       $('#devicehere').load($(this).attr('href')+" #main_content", function() {
