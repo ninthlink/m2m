7795391299376d1a862041d9186ca2dcc2beacc2
alex chousmith <alex@ninthlink.com>
2015-09-22 17:17:50 +0000

    change default tab to Thumb not List

--- sites/all/themes/m2m2/js/m2m_devicelist.js
+++ sites/all/themes/m2m2/js/m2m_devicelist.js
@@ -43,11 +43,11 @@ function hide_grid() {
 (function($) {
 	Drupal.behaviors.m2m_devicelist = function(context) {
 		if ( $('.devicelist').size() > 0 ) {
-			if(view_type == 'thumbs') {
+			//if(view_type == 'thumbs') {
 				show_grid();
-			} else {
-				hide_grid();
-			}
+			//} else {
+			//	hide_grid();
+			//}
       /*
 			$('.view-content .field-content a').click(function() {
 				$('#devicehere').load($(this).attr('href')+" #main_content", function() {
